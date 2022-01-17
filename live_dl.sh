2c47ee06844746fc3485a150996b76fe6535c00a
FappyMcFap <blopmiyers@protonmail.com>
2022-01-17 02:35:57 +0000

    Update live_dl.sh

--- live_dl.sh
+++ live_dl.sh
@@ -5,7 +5,7 @@
 # Probably messy but does the job :)
 # Made by FappyMcFap January 03 2022
 
-# Specify the time you'd like script to end at line 35
+# Specify the time you'd like script to end at line 33
 # Configure variables: streamer & path
 
 curdate=$(date +"%Y-%m-%d")
@@ -22,18 +22,25 @@ fi
 /usr/local/bin/yt-dlp -o ''$path'/%(description)s-%(id)s_%(upload_date)s.%(ext)s' --hls-use-mpegts $link 2>&1 | tee "$path/$curdate.log"
 
 # Not pretty but uses grep to detect yt-dlp's error message that contains 'offline'
-# Tries again after 2m if stream is offline
 while cat $path/$curdate.log | grep -i 'offline'; do
 
 	# Sleep for 2m before retrying..
 	sleep 2m
 	# Update the current time
 	curtime=$(date +%H:%M)
-	/usr/local/bin/yt-dlp -o ''$path'/%(description)s-%(id)s_%(upload_date)s.%(ext)s' --hls-use-mpegts $link 2>&1 | tee "$path/$curdate.log"
 
-	# End script if time is passed 21:00
+	# If time = 21:00 end script
 	if [[ "$curtime" = "21:00" ]]; then
 		break
-	fi
+	else
+		/usr/local/bin/yt-dlp -o ''$path'/%(description)s-%(id)s_%(upload_date)s.%(ext)s' --hls-use-mpegts $link 2>&1 | tee "$path/$curdate.log"
+	
+		# Delete the log file when we don't need it anymore
+		rm "$path/$curdate.log"
 
+		# Convert leftover .part file to mp4 
+		for file in *.part; do
+			mv "$file" "${file%.part}.mp4"
+		done
+	fi
 done
